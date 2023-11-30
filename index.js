32464361a9405a11fc484ba2acfc4e80ad0e9ce2
Brenagu <142752782+Brenagu@users.noreply.github.com>
2023-11-30 15:21:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 /**Index.js tiene que establecer la configuración del server */
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
 import express from 'express';
+import { resolve } from 'path';
 import mainRouter from './src/routes/main-routes.js';
 import shopRouter from './src/routes/shop-routes.js';
-import adminRouter from './src/routes/admin-routes.js'; 
+import adminRouter from './src/routes/admin-routes.js';
 
+dotenv.config()
 const app = express();
-const PORT = 8080;
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const PORT = process.env.PORT || 8080;
+const __dirname = resolve();
 
 // set the view engine
 app.set('view engine', 'ejs');
@@ -16,10 +17,13 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + "/src/views");
 
 // middlewares
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(express.static('public'));
-app.use("/", mainRouter);
-app.use("/shop", shopRouter);
-app.use("/admin", adminRouter);
+app.use("/", mainRouter); // --> http://localhost:3000/
+app.use("/shop", shopRouter); // --> http://localhost:3000/shop
+app.use("/admin", adminRouter); // --> http://localhost:3000/admin
+app.use("/edit",adminRouter);
 
 // levantar servidor
 app.listen(PORT, () => {
